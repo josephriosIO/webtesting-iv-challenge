929c008758d84efd59deb5e9bb10ac2cdee42253
josephriosio <blankworldwide@gmail.com>
2019-05-16 13:40:59 -0700

    intital setup

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const server = require("./api/server");
+
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`\n *** running on port:${port} ***\n`);
+});
